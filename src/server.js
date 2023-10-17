e664792326774ad7e6c962911c405bc9376279dd
firstaple <firstaple@firstaple>
2023-10-18 00:54:34 +0900

    External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const loggerMiddleware = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
   return res.send("I love middlewares");
 };
 
-app.get("/", logger, handleHome);
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(loggerMiddleware);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
