39b01cdd5e79d4678dbc41b9d4793623e4479522
firstaple <firstaple@firstaple>
2023-10-20 00:41:40 +0900

    MVP Styles

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,4 @@
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", potato: "tomato" });
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
