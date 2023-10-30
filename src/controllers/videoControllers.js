6d71f1b95c05bdf276f6689bc2f722f99d525a03
firstaple <firstaple@gmail.com>
2023-10-30 21:55:31 +0900

    Our First Query ( Async Await )

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,47 +1,25 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comment: 2,
-    createAt: "2 minuter ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comment: 2,
-    createAt: "2 minuter ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comment: 2,
-    createAt: "2 minuter ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    console.log("videos", videos);
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    res.render("Server-error", { error });
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -49,14 +27,5 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comment: 0,
-    createAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
