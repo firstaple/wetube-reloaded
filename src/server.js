2ddc14f7c7e587bc394f3e612b74be134dc89392
firstaple <firstaple@gmail.com>
2023-11-09 11:50:48 +0900

    Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import globalRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,6 +12,32 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+});
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
