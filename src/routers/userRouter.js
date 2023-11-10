2b5d1b63e93974d9fe073c9b29df365b904b6790
firstaple <firstaple@gmail.com>
2023-11-10 16:38:51 +0900

    Protector and Public Middlewares

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,13 +8,14 @@ import {
   see,
   startGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddlewware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddlewware, logout);
+userRouter.route("/edit").all(protectorMiddlewware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
