dab4d9816e1c311273ded9c7a7a34b8e30150374
firstaple <firstaple@gmail.com>
2023-12-10 02:05:59 +0900

    Video Upload

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -11,9 +11,9 @@ import {
   startGithubLogin,
 } from "../controllers/userController";
 import {
+  avatarUpload,
   protectorMiddleware,
   publicOnlyMiddleware,
-  uploadFiles,
 } from "../middlewares";
 
 const userRouter = express.Router();
@@ -24,7 +24,7 @@ userRouter
   .route("/edit")
   .all(protectorMiddleware)
   .get(getEdit)
-  .post(uploadFiles.single("avatar"), postEdit);
+  .post(avatarUpload.single("avatar"), postEdit);
 
 userRouter
   .route("/change-password")
